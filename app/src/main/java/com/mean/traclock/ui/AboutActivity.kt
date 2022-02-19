630218833178e0d77ff89117085f2a3c1013745d
Mean <390602272@qq.com>
2022-02-19 22:18:31 +0800

    ✨ feat(About): 初步完成About页面

--- app/src/main/java/com/mean/traclock/ui/AboutActivity.kt
+++ app/src/main/java/com/mean/traclock/ui/AboutActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -13,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -20,6 +24,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mean.traclock.BuildConfig
 import com.mean.traclock.R
+import com.mean.traclock.App
 import com.mean.traclock.ui.theme.TraclockTheme
 
 class AboutActivity : ComponentActivity() {
@@ -29,7 +34,6 @@ class AboutActivity : ComponentActivity() {
         setContent {
             TraclockTheme {
                 val scrollBehavior = remember { TopAppBarDefaults.pinnedScrollBehavior() }
-
                 Scaffold(topBar = {
                     SmallTopAppBar(
                         navigationIcon = {
@@ -41,7 +45,7 @@ class AboutActivity : ComponentActivity() {
                         scrollBehavior = scrollBehavior
                     )
                 }) {
-                    Test()
+                    Content()
                 }
             }
         }
@@ -49,9 +53,9 @@ class AboutActivity : ComponentActivity() {
 }
 
 @Composable
-@Preview
-fun Test() {
-    Column {
+@Preview(showSystemUi = true)
+fun Content() {
+    Column(Modifier.verticalScroll(rememberScrollState())) {
         Image(
             painterResource(R.drawable.ic_launcher_foreground),
             stringResource(R.string.app_name),
@@ -72,5 +76,28 @@ fun Test() {
             modifier = Modifier.fillMaxWidth()
         )
         Divider(Modifier.padding(vertical = 16.dp))
+        Text(
+            "开发者",
+            style = MaterialTheme.typography.labelLarge,
+            modifier = Modifier.padding(horizontal = App.horizontalMargin)
+        )
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painterResource(R.drawable.avatar),
+                stringResource(R.string.developer_avatar),
+                modifier = Modifier
+                    .padding(App.horizontalMargin, 12.dp)
+                    .height(46.dp)
+                    .clip(CircleShape)
+            )
+            Column(verticalArrangement = Arrangement.spacedBy(2.dp)) {
+                Text("Mean",style = MaterialTheme.typography.titleMedium,)
+                Text(
+                    stringResource(R.string.developer_introduction),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+        }
     }
 }
