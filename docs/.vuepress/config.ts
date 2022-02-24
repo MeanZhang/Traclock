3a7ba75dd360286ea395d46b1f1055e67179832b
Mean <390602272@qq.com>
2022-02-24 23:12:56 +0800

    📃 docs: 增加帮助文档

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,35 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+    lang: 'zh-CN',
+    title: '时迹',
+    description: '时迹帮助文档',
+    head: [
+        ['link', { rel: 'icon', href: '/images/logo.svg' }],
+        ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['link', { rel: 'apple-touch-icon', href: '/images/icons/apple-touch-icon-152x152.png' }],
+        ['link', { rel: 'mask-icon', href: '/images/logo.svg', color: '#5bbad5' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#2d89ef' }]
+    ],
+
+    themeConfig: {
+        logo: '/images/logo.png',
+        repo: 'MeanZhang/Traclock',
+        docsBranch: "main",
+        docsDir:"docs",
+        editLinkText: "编辑此页面"
+    },
+
+    // plugins: [
+    //     [
+    //         '@vuepress/pwa',
+    //         {
+    //             skipWaiting: true,
+    //         },
+    //     ],
+    // ],
+})
---
+++ docs/README.md
@@ -0,0 +1,10 @@
+---
+home: true
+heroImage: /images/logo.svg
+heroText: 时迹
+tagline: 一款记录各项任务用时的工具
+actions:
+  - text: 查看帮助
+    link: /help
+footer: Copyright © 2021-2022 Mean
+---
