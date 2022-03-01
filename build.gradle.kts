4732ccb8d3c011ec540bad817b197bc27e687a0f
Mean <390602272@qq.com>
2022-03-01 21:04:11 +0800

    🐳 chore: 将构建配置从 Groovy 迁移到 KTS

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+buildscript {
+    extra.apply{
+        set("compose_version", "1.2.0-alpha04")
+    }
+}
+
+//allprojects {
+//    ext {
+//        set("compose_version", "1.2.0-alpha04")
+//    }
+//}
+
+plugins {
+    id("com.android.application") version "7.2.0-alpha06" apply false
+    id("com.android.library") version "7.2.0-alpha06" apply false
+    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
+}
+
+tasks.register("clean", Delete::class.java) {
+
+    delete(rootProject.buildDir)
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven { setUrl("https://maven.aliyun.com/repository/google") }
+        maven { setUrl("https://maven.aliyun.com/repository/public") }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { setUrl("https://jitpack.io") }
+        maven { setUrl("https://maven.aliyun.com/repository/google") }
+        maven { setUrl("https://maven.aliyun.com/repository/public") }
+  }
+}
+rootProject.name = "Traclock"
+include(":app")
