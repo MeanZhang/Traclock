eff2fc1dfaeec77b38e0edac9884573020b08886
Mean <390602272@qq.com>
2022-04-18 23:26:57 +0800

    ✨ feat: 修改开始按钮样式

--- app/src/main/kotlin/com/mean/traclock/ui/components/SmallOutlinedButton.kt
+++ app/src/main/kotlin/com/mean/traclock/ui/components/SmallOutlinedButton.kt
@@ -1,76 +1,92 @@
 package com.mean.traclock.ui.components
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Chip
+import androidx.compose.material.ChipDefaults
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mean.traclock.R
 
 // @Composable
-// fun SmallOutlinedButton(text:String,onClick:()->Unit){
+// fun SmallOutlinedButton(text: String, onClick: () -> Unit) {
 //    OutlinedButton(
 //        onClick = onClick,
-//        modifier = Modifier.height(28.dp)
+//        border = BorderStroke(
+//            width = 1.dp,
+//            // 边框颜色
+//            color = MaterialTheme.colorScheme.outline.copy(0.2f)
+//        ),
+//        colors = ButtonDefaults.outlinedButtonColors(
+//            contentColor = MaterialTheme.colorScheme.outline,
+//            containerColor = MaterialTheme.colorScheme.inverseOnSurface
+//        ),
+//        modifier = Modifier.height(32.dp)
 //    ) {
-//        Row(verticalAlignment = Alignment.CenterVertically) {
+//        Row(
+//            verticalAlignment = Alignment.CenterVertically,
+//            horizontalArrangement = Arrangement.spacedBy(8.dp)
+//        ) {
 //            Icon(
 //                imageVector = Icons.Default.PlayArrow,
 //                contentDescription = stringResource(R.string.start),
-// //                modifier = Modifier.size(16.dp)
+//                modifier = Modifier.size(16.dp)
 //            )
-//            Text(
-//                text,
-// //                style = MaterialTheme.typography.labelMedium
-//            )
+//            Text(text)
 //        }
 //    }
 // }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SmallOutlinedButton(text: String, onClick: () -> Unit = {}) {
-    Surface(
-        shape = CircleShape,
+    Chip(
+        modifier = Modifier.height(28.dp),
+        onClick = onClick,
+        colors = ChipDefaults.chipColors(
+            backgroundColor = MaterialTheme.colorScheme.inverseOnSurface,
+            contentColor = MaterialTheme.colorScheme.outline.copy(0.2f),
+            leadingIconContentColor = MaterialTheme.colorScheme.outline.copy(0.2f)
+        ),
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.PlayArrow,
+                contentDescription = stringResource(R.string.start),
+                modifier = Modifier.padding(start = 6.dp).size(16.dp),
+                tint = MaterialTheme.colorScheme.outline
+            )
+        },
         border = BorderStroke(
             width = 1.dp,
             // 边框颜色
             color = MaterialTheme.colorScheme.outline.copy(0.2f)
         ),
-        modifier = Modifier.clickable(onClick = onClick),
-        // 图标和文字颜色
-        contentColor = MaterialTheme.colorScheme.secondary.copy(0.8f),
-        // 按钮背景颜色
-        color = MaterialTheme.colorScheme.inverseOnSurface.copy(0.6f),
     ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Icon(
-                imageVector = Icons.Default.PlayArrow,
-                contentDescription = stringResource(R.string.start),
-                modifier = Modifier
-                    .size(24.dp)
-                    .padding(vertical = 4.dp)
-            )
-            Text(
-                text = text,
-                style = MaterialTheme.typography.labelMedium,
-                fontFamily = FontFamily.Monospace,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(end = 6.dp)
-            )
-        }
+        Text(
+            text = text,
+            style = MaterialTheme.typography.labelMedium,
+            fontFamily = FontFamily.Monospace,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.outline
+        )
     }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun Preview() {
+    SmallOutlinedButton(text = "00:00:00") {}
 }
