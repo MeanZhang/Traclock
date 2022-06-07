400f666a07d74971ce63de1207bb4902cd0ef780
Mean <390602272@qq.com>
2022-06-07 09:10:09 +0800

    🔧 build: 更新AGP至7.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
 // }
 
 plugins {
-    val agpVersion = "7.2.0"
+    val agpVersion = "7.2.1"
     id("com.android.application") version agpVersion apply false
     id("com.android.library") version agpVersion apply false
     id("org.jetbrains.kotlin.android") version "1.6.21" apply false
