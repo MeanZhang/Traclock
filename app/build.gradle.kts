13df6eb7f70efc0d34bda76daa5997f738b8dbbe
Mean <390602272@qq.com>
2022-06-17 07:16:48 +0800

    🐎 ci: 更新Compose至1.2.0-rc01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-val composeVersion by extra("1.2.0-beta03")
+val composeVersion by extra("1.2.0-rc01")
 
 plugins {
     id("com.android.application")
@@ -66,7 +66,7 @@ android {
 
 dependencies {
     val roomVersion = "2.4.2"
-    val accompanistVersion = "0.24.10-beta"
+    val accompanistVersion = "0.24.11-rc"
     val workVersion = "2.7.1"
     val navVersion = "2.4.1"
 
