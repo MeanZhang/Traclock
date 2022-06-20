c2035a55d664be220439434a42d4dcf738081835
Mean <390602272@qq.com>
2022-06-20 09:59:53 +0800

    ✨ feat: 更新Target API至33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 }
 
 android {
-    compileSdk = 32
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.mean.traclock"
         minSdk = 21
-        targetSdk = 32
+        targetSdk = 33
         versionCode = 1
         versionName = "0.6.0"
 
