4fdb0887f7dec8f14d274de89715c9ee349dcff0
Mean <390602272@qq.com>
2022-10-25 00:16:48 +0800

    style: 删除多余import

--- app/src/main/kotlin/com/mean/traclock/ui/screens/Settings.kt
+++ app/src/main/kotlin/com/mean/traclock/ui/screens/Settings.kt
@@ -11,7 +11,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Feedback
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.SettingsBackupRestore
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
